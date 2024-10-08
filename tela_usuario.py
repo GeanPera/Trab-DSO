cfef1be1775ec4fca41224ae3bad2467f09f950b
Gean Pereira <geanp578@gmail.com>
2024-10-07 21:11:51 -0300

    add tela usuario

---
+++ tela_usuario.py
@@ -0,0 +1,24 @@
+
+
+
+class TelaUsuario():
+    def opcoes_tela(self):
+        print("-------- Tela Usuario ----------")
+        print("Escolha a opcao")
+        print("1 - Cadastrar")
+        print("2 - Alterar")
+        print("3 - Excluir")
+        print("4 - Adicionar Amigo")
+        print("5 - Excluir Amigo")
+        print("6 - Depositar Saldo")
+        print("0 - Retornar")
+
+    opcao = int(input("Escolha a opcao: "))
+    return opcao
+
+    def dados_usuario():
+        pass
+
+    def alterar_usuario():
+        pass
+    
