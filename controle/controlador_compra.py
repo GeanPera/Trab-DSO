b5ac989614ebdb17b47e8fb60ea144f1530eb7e1
Gean Pereira <geanp578@gmail.com>
2024-11-01 21:31:36 -0300

    metodo presentear atualizado

--- controle/controlador_compra.py
+++ controle/controlador_compra.py
@@ -27,11 +27,12 @@ class ControladorCompra:
         jogo_escolhido = self.__controlador_sistema.controlador_jogos.jogo(nome_jogo)
         my_nickname = self.__tela_compra.pede_nickname("Qual seu Nickname? ")
         usuario = self.__controlador_sistema.controlador_usuarios.encontrar_usuario(my_nickname)
-        amigo_nickname = self.__tela_compra.mostra_mensagem("")
+        amigo_nickname = self.__tela_compra.mostra_mensagem("Qual o Nickname do seu amigo? ")
+        amigo = self.__controlador_sistema.controlador_usuarios.encontrar_usuario(amigo_nickname)
         if jogo_escolhido:
             if usuario:
                 if usuario.saldo >= jogo_escolhido.preco:
-                    self.__controlador_sistema.controlador_usuarios.adicionar_jogo(jogo_escolhido, usuario)
+                    self.__controlador_sistema.controlador_usuarios.presentear_amigo(jogo_escolhido, amigo)
                     self.__tela_compra.mostra_mensagem(f"Presente enviado com sucesso. Saldo atual: {usuario.saldo}")
             else:
                 self.__tela_compra.mostra_mensagem("Usuário não encontrado.")            
