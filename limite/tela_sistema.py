620d25e32b1a2a70c3ab5ace8d40c596506e1d96
Luan Andrade <96707728+Cellmander@users.noreply.github.com>
2024-11-25 21:19:42 -0300

    tela sistema

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,17 +1,42 @@
+import PySimpleGUI as sg
+
 class TelaSistema:
+    def __init__(self):
+        self.__window = None
+        self.init_components()
+        
     def tela_opcoes(self):
-        while True:
-            print("-------- Tela Sistemas ---------")
-            print("Escolha sua opcao")
-            print("1 - Menu da Loja")
-            print("2 - Usuário")
-            print("3 - Relatório")
-            print("0 - Sair")
-            try:
-                opcao = int(input("Escolha uma opção: "))
-                return opcao
-            except ValueError:
-                self.mostra_mensagem("Entrada inválida! Por favor, insira um número correspondente à opção.")
+        self.init_components()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if button in (None, 'Cancelar'):
+            opcao = 0
+
+        self.close()
+        return opcao
+
+            
+    def close(self):
+        self.__window.Close()
+                
+    def init_components(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('DarkAmber')
+        layout = [
+            [sg.Text('SIN Games', font=("Verdana", 25, ), colors='Cyan')],
+            [sg.Text('Escolha uma opção!', font=("Verdana", 18))],
+            [sg.Radio('Menu Loja', 'RD1', key ='1')],
+            [sg.Radio('Usuário', 'RD1', key ='2')],
+            [sg.Radio('Relatório', 'RD1', key='3')],
+            [sg.Button('Confirmar', button_color='Green'), sg.Cancel('Encerrar sessão', button_color='Red')]
+        ]
+        self.__window = sg.Window('SIN Games').Layout(layout)
 
     def mostra_mensagem(self, mensagem):
         print(f"{mensagem}")
